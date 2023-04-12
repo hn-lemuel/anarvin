1a922c39fa1e545c5a5550a60db9f770804e6006
hn-lemuel <Lemuel.Lloren@au.harveynorman.com>
2023-04-12 14:27:11 +0800

    Set up cart notification

--- assets/add-to-cart.js
+++ assets/add-to-cart.js
@@ -1,31 +1,77 @@
 class AddToCartButton extends HTMLElement {
+  #productImgEl;
+  #productTitleEl;
+  #productQuantityEl;
+  #productPriceEl;
+  #modalEl;
+
   constructor() {
     super();
+    this.#productImgEl = document.querySelector(".product-image");
+    this.#productTitleEl = document.querySelector(".product-title");
+    this.#productQuantityEl = document.querySelector(".product-quantity");
+    this.#productPriceEl = document.querySelector(".product-price");
+    this.#modalEl = document.querySelector(".cart-notification-modal");
     this.addEventListener("click", this._addToCart.bind(this));
+  }
+
+  _formatPrice(price) {
+    price = price.replace(",", "");
+    let numPrice = parseInt(price, 10) / 100;
+    let formattedPrice = numPrice.toFixed(2);
+    let priceWithCurrency = `$${formattedPrice}`;
+    return priceWithCurrency;
   }
 
   async _addToCart(event) {
     event.preventDefault();
     let addToCartForm = document.querySelector("#product-form");
     let formData = new FormData(addToCartForm);
-    fetch(window.Shopify.routes.root + "cart/add.js", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .catch((error) => {
-        console.error("Error:", error);
+    const cart = "cart/add.js";
+
+    try {
+      const response = await fetch(`${window.Shopify.routes.root}${cart}`, {
+        method: "POST",
+        body: formData,
       });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      this._updateProductData(data);
+      this._showCartNotification();
+    } catch (error) {
+      console.error("Error:", error);
+    }
   }
 
-  open() {
-    this.style.display = "block";
+  _updateProductData(data) {
+    const { featured_image, product_title, quantity, price } = data;
+    this.#productImgEl.src =
+      featured_image?.url ||
+      "{{ 'product-1' | placeholder_svg_tag: 'w-20 h-20 object-cover border' }}";
+    this.#productImgEl.alt = featured_image?.alt || "Product title";
+    this.#productTitleEl.textContent = product_title;
+    this.#productQuantityEl.textContent = quantity;
+    this.#productPriceEl.textContent = this._formatPrice(
+      price?.toString() || ""
+    );
   }
 
-  close() {
-    this.style.display = "none";
+  _showCartNotification() {
+    this.#modalEl.classList.remove("hidden");
+    const closeModalButton = (this.#modalEl = document.querySelector(
+      ".cart-notification-modal"
+    ));
+    closeModalButton.addEventListener(
+      "click",
+      this._closeCartNotification.bind(this)
+    );
+  }
+
+  _closeCartNotification() {
+    console.log("is click");
+    this.#modalEl.classList.add("hidden");
   }
 }
 
